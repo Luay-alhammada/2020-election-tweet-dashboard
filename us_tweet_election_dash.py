bfdd6972d5f33a1de3a6a69576783b7be70ddf8b
Luay-alhammada <alhammada.luay@gmail.com>
2025-01-10 14:20:48 +0100

    Update us_tweet_election_dash.py

--- us_tweet_election_dash.py
+++ us_tweet_election_dash.py
@@ -34,7 +34,7 @@ title = "<h1 style='text-align:center;'>US Election 2020 Biden VS Trump</h1>"
 
 
 
-col1, col2,col3 = st.columns([1.5, 1.5]) 
+col1, col2,col3 = st.columns([1.5,5,1]) 
 #  unique dates list
 with col1:
   st.markdown('<h1 style="color: IndianRed;font-size: 30px;">Map Configuration</h1>', unsafe_allow_html=True)
